e09a9d72a75fadcbed2d800a54568f11e5b30ac8
RJ Mohammad <rjmohammad@outlook.com>
2017-05-02 01:06:37 -0400

    wrote project component

---
+++ src/app/components/project.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit } from '@angular/core';
+
+import { Project } from '../services/project';
+import { ProjectsService } from '../services/projects.service';
+
+@Component({
+  selector: 'axial-projects',
+  templateUrl: '../templates/project.component.html'
+})
+
+export class ProjectsComponent implements OnInit {
+  projects: Project[];
+
+  constructor(
+    private projService: ProjectsService
+  ) { }
+
+  getProjects() : void {
+    this.projService.getProjects().then(projs => this.projects = projs);
+  }
+
+  ngOnInit() : void {
+    this.getProjects
+  }
+}
