6243890dc0d6ff7d0bd3a5e3221c22c25fd6f2ba
RJ Mohammad <rjmohammad@outlook.com>
2017-05-03 16:42:39 -0400

    import router and set up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './components/app.component';
 import { ProjectsComponent } from './components/project.component'
 import { ProjectsService } from './services/projects.service';
-
+import { AppRoutingModule } from './routes/app-routing.module'
 @NgModule ({
   imports: [
     BrowserModule,
---
+++ src/app/routes/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProjectsComponent } from '../components/project.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard'},
+  { path: 'dashboard', component: ProjectsComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {};
